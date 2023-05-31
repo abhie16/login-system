e8d311206e4ac45c9e86a07554adb01d35d1a191
Abhishek pandey <abhie9336@gmail.com>
2023-05-31 19:38:41 +0530

    add style

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
 
 const PORT = 3000 || process.env.PORT;
@@ -8,6 +9,9 @@ const app = express();
 const server = http.createServer(app);
 
 app.set('view engine','ejs');
+
+//static
+app.use('/static',express.static(path.join(__dirname,'public')));
 
 app.get('/',(req, res)=>{
     res.render('base',{title:"Login System"})
