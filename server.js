d41d433e46014e9eaa26de8f4ea9f0c6f4f823c5
Abhishek pandey <abhie9336@gmail.com>
2023-06-02 18:57:08 +0530

    add login function

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const {v4:uuidv4} = require('uuid');
+
+const router = require('./router');
 
 const PORT = 3000 || process.env.PORT;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 const server = http.createServer(app);
 
@@ -13,9 +21,17 @@ app.set('view engine','ejs');
 //static
 app.use('/static',express.static(path.join(__dirname,'public')));
 
+app.use(session({
+    secret: uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}))
+
+
 app.get('/',(req, res)=>{
     res.render('base',{title:"Login System"})
 })
+app.use('/route',router);
 
 app.listen(PORT , ()=>{
     console.log(`server is running at http://localhost:${PORT}`)
