181656a30c06b48c8438dbaa341382fcad23f557
Abhishek pandey <abhie9336@gmail.com>
2023-06-03 17:56:10 +0530

    add logout functionality

--- router.js
+++ router.js
@@ -11,13 +11,37 @@ const credential = {
 router.post('/login',(req,res)=>{
     if(req.body.email === credential.email && req.body.password === credential.password ){
         req.session.user = req.body.email;
-        //res.redirect('/dashboard');
-        res.end('successfull');
+        res.redirect('/route/dashboard');
+        //res.end('successfull');
     }
     else{
         res.end("Invalid Username")
     }
 });
 
+
+router.get('/dashboard',(req,res)=>{
+    if(req.session.user){
+        res.render('dashboard', {user: req.session.user});
+    }
+    else{
+        res.send("Unauthorized User");
+    }
+})
+
+
+//route for logout
+
+router.get('/logout',(req,res)=>{
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+            res.send("Error");
+        }
+        else{
+            res.render('base',{title:"Express", logout:"logout successfully"})
+        }
+    });
+})
 
 module.exports = router;
